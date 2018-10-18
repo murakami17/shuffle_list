f5711ec4b5d61a99f9d740f363a968d206204983
Taku MURAKAMI <murakami.taku.17@shizuoka.ac.jp>
2018-10-18 12:00:00 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='shuffle_list',
+    version='0.1.0',
+    description='Shuffle and put icons with given distributions',
+    long_description=readme,
+    author='Taku MURAKAMI',
+    author_email='murakami.taku.17@shizuoka.ac.jp',
+    url='https://github.com/murakami17/shuffule_list',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
