80834c43bf75af9a26a04aafcff6907ff96815f8
Taku MURAKAMI <murakami.taku.17@shizuoka.ac.jp>
2018-10-22 17:25:29 +0900

    Update main method

--- src/core.py
+++ src/core.py
@@ -86,5 +86,5 @@ if __name__ == "__main__":
     ]
     
     for index, shuffle_list in enumerate(shuffle_list_list):
-        print(str(index) + ":" + shuffle_list.put_shuffled_list() + "\n")
+        print(str(index) + ":\n" + shuffle_list.put_shuffled_list() + "\n")
     
